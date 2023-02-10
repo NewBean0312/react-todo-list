34e81c6eb684d44d143b236a52472c94b69333ed
NewBean0312 <newbean0312@gmail.com>
2023-02-10 22:40:39 +0900

    페이지 분리 및 초기 UI 작업 2

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,19 @@
+import TodosEmpty from "../components/TodosEmpty";
+
 export default function MainPage() {
-    return (
-      <>
-        <h1>메인 페이지</h1>
-      </>
-    );
+  const todosEmpty = true;
+
+  if (todosEmpty) {
+    return <TodosEmpty />;
   }
+
+  return (
+    <>
+      <div className="flex-1 flex justify-center items-center">
+        <div>
+          <span>메인 페이지</span>
+        </div>
+      </div>
+    </>
+  );
+}
