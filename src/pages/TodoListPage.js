04276e844f4f827bda31976cad0d44193572b394
NewBean0312 <newbean0312@gmail.com>
2023-02-15 17:25:16 +0900

    탭 메뉴에 따라 필터링 처리

--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -15,6 +15,20 @@ export default function TodoList() {
     TodoList__filterCompletedIndexAtom
   );
 
+  const getFilteredTodos = () => {
+    if (filterCompletedIndex == 1) {
+      return todosStatus.todos.filter((todo) => !todo.completed);
+    }
+
+    if (filterCompletedIndex == 2) {
+      return todosStatus.todos.filter((todo) => todo.completed);
+    }
+
+    return todosStatus.todos;
+  };
+
+  const filteredTodos = getFilteredTodos();
+
   return (
     <>
       <TodoOptionDrawer status={todoOptionDrawerStatus} />
@@ -55,7 +69,7 @@ export default function TodoList() {
 
       <div className="mt-4 px-4">
         <ul>
-          {todosStatus.todos.map((todo, index) => (
+          {filteredTodos.map((todo, index) => (
             <TodoListItem
               key={todo.id}
               todo={todo}
